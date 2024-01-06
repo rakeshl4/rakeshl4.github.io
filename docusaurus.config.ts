0e89ecc2d5b702eb72c394be59115579697b821a
Rakesh Lakshminarayana <rakeshl84@outlook.com>
2024-01-06 13:08:50 +1100

    update blog files

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -64,12 +64,12 @@ const config: Config = {
       //   src: 'img/logo.svg',
       // },
       items: [
-        {
-          type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
-          position: 'left',
-          label: 'Tutorial',
-        },
+        // {
+        //   type: 'docSidebar',
+        //   sidebarId: 'tutorialSidebar',
+        //   position: 'left',
+        //   label: 'Tutorial',
+        // },
         {to: '/blog', label: 'Blog', position: 'left'},
         {
           href: 'https://github.com/facebook/docusaurus',
