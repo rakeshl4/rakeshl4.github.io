199e889d1531b4516d0ca5e7ee8f120c88f27755
Rakesh Lakshminarayana <rakesh.lakshminarayana@insight.com>
2024-08-26 11:20:42 +1000

    update site name

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,8 +1,8 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'BytesBlogger',
+  title: 'BinaryTrails',
   author: 'Rakesh Lakshminarayana',
-  headerTitle: 'BytesBlogger',
+  headerTitle: 'BinaryTrails',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
